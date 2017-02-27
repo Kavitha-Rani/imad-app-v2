6ceec7bd06097f448625f0190962f16de52c8203
Kavitha Rani M R <Kavitha-Rani@users.noreply.github.com>
2017-02-27 06:00:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,8 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 //To move the image
-var img = document.getElementById('madi');
-img onclick = function(){
-    img.style.marginleft =100px;
+var img=document.getElementById('madi');
+img.onclick = function()
+{
+    img.style.marginLeft =100px;
 };

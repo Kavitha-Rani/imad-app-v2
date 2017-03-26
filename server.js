4452fbd076c1548af879359c91870893da692c01
Kavitha Rani M R <Kavitha-Rani@users.noreply.github.com>
2017-03-26 06:23:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,93 +5,93 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-// var articles= {
-//     'article1':{
-//         title : 'Article One',
-//         heading : 'Article One',
-//         date : 'Feb 23 2017',
-//         content :`<p> 
-//               This is article one.This is the content of article 1.
-//               </p> 
-//               <p> 
-//               This is article one.This is the content of article 1.
-//               </p> 
-//               <p> 
-//               This is article one.This is the content of article 1. 
-//               </p>`
-//     },
-//     'article2':{
-//         title : 'Article Two',
-//         heading : 'Article Two',
-//         date : 'Feb 23 2017',
-//         content :`<p> 
-//               This is article Two.This is the content of article 2.
-//               </p> 
-//               <p> 
-//               This is article Two.This is the content of article 2.
-//               </p> 
-//               <p> 
-//               This is article Two.This is the content of article 2. 
-//               </p>`
-//     },
-//     'article3':{
-//         title : 'Article Three',
-//         heading : 'Article Three',
-//         date : 'Feb 23 2017',
-//         content :`<p> 
-//               This is article Three.This is the content of article 3.
-//               </p> 
-//               <p> 
-//               This is article Three.This is the content of article 3.
-//               </p> 
-//               <p> 
-//               This is article Three.This is the content of article 3. 
-//               </p>`
-//     }
-// };
+var articles= {
+    'article1':{
+        title : 'Article One',
+        heading : 'Article One',
+        date : 'Feb 23 2017',
+        content :`<p> 
+              This is article one.This is the content of article 1.
+              </p> 
+              <p> 
+              This is article one.This is the content of article 1.
+              </p> 
+              <p> 
+              This is article one.This is the content of article 1. 
+              </p>`
+    },
+    'article2':{
+        title : 'Article Two',
+        heading : 'Article Two',
+        date : 'Feb 23 2017',
+        content :`<p> 
+              This is article Two.This is the content of article 2.
+              </p> 
+              <p> 
+              This is article Two.This is the content of article 2.
+              </p> 
+              <p> 
+              This is article Two.This is the content of article 2. 
+              </p>`
+    },
+    'article3':{
+        title : 'Article Three',
+        heading : 'Article Three',
+        date : 'Feb 23 2017',
+        content :`<p> 
+              This is article Three.This is the content of article 3.
+              </p> 
+              <p> 
+              This is article Three.This is the content of article 3.
+              </p> 
+              <p> 
+              This is article Three.This is the content of article 3. 
+              </p>`
+    }
+};
 
-// function createTemplate(data){
-//     var title = data.title;
-//     var date = data.date;
-//     var heading = data.heading;
-//     var content = data.content;
-//     var htmlTemplate=`<html>
-//           <head> 
-//           <title>${title}</title>
-//             <meta name="viewport" content="width=device-width, initial-scale=1">
-//             <link href ="/ui/style.css" rel="stylesheet"/>
-//           </head>
-//           <body>
-//             <div class="container">
-//               <div>
-//               <a href="/">Home</a>
-//               </div>
-//               <hr/>
-//               <h3>
-//                   ${heading}
-//               </h3>
-//               <div>
-//                   ${date}
-//               </div>
-//               <div>
-//                   ${content}
-//               </div>
-//             </div>
-//           </body>
-//          </html>`;
-// return htmlTemplate;
-// }
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate=`<html>
+          <head> 
+          <title>${title}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href ="/ui/style.css" rel="stylesheet"/>
+          </head>
+          <body>
+            <div class="container">
+              <div>
+              <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date}
+              </div>
+              <div>
+                  ${content}
+              </div>
+            </div>
+          </body>
+         </html>`;
+return htmlTemplate;
+}
 
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-// app.get('/:articleName', function (req, res) { //articleName = article1
-//   //article[articleName]={}
-//   var articleName = req.params.articleName;
-//   res.send(createTemplate(articles[articleName]));
-// });
+app.get('/:articleName', function (req, res) { //articleName = article1
+  //article[articleName]={}
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
 
 var counter=0;
 app.get('/counter', function (req, res){
